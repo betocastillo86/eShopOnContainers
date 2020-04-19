905e27bf0ddff26ba31e90178c0d53f481a17bac
betocastillo86 <castillopradagabriel@gmail.com>
2020-04-19 12:41:10 -0500

    Added POST and GET BY ID for comments controller

--- src/Services/Catalog/Catalog.API/Controllers/CommentController.cs
+++ src/Services/Catalog/Catalog.API/Controllers/CommentController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.eShopOnContainers.Services.Catalog.API.Infrastructure;
 using Microsoft.eShopOnContainers.Services.Catalog.API.Model;
 using Microsoft.eShopOnContainers.Services.Catalog.API.ViewModel;
+using Shop.Common.Infrastrucutre;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -48,6 +50,43 @@ namespace Catalog.API.Controllers
             var model = new PaginatedItemsViewModel<Comment>(pageIndex, pageSize, totalComments, comments);
 
             return this.Ok(model);
+        }
+
+        [Route("{id:int}")]
+        [HttpGet]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult> GetCommentById(int itemId, int id)
+        {
+            var comment = await this.context.Comments
+                .Include(c => c.CatalogItem)
+                .FirstOrDefaultAsync(c => c.Id == id && c.CatalogItemId == itemId);
+
+            if (comment == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.Ok(comment);
+        }
+
+        [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.Created)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult> CreateComment(int itemId, [FromBody]Comment model)
+        {
+            var item = new Comment
+            {
+                CatalogItemId = itemId,
+                Text = model.Text,
+                UserId = model.UserId
+            };
+
+            this.context.Comments.Add(item);
+
+            await this.context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCommentById), new { id = item.Id }, null);
         }
     }
 }
